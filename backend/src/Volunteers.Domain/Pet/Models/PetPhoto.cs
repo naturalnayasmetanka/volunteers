a1ff99b3039e1e822eb5051d00223c4d0c5122c2
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-11 14:18:35 +0300

    B-4

---
+++ backend/src/Volunteers.Domain/Pet/Models/PetPhoto.cs
@@ -0,0 +1,8 @@
+﻿namespace Volunteers.Domain.Pet.Models
+{
+    public class PetPhoto
+    {
+        public string Path { get; set; } = default!;
+        public bool IsMain { get; set; }
+    }
+}
