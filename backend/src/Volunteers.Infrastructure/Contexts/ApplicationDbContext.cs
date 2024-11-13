4648187493f2c0c320ca9007a9d5f90e751831bd
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-13 17:28:17 +0300

    update context

---
+++ backend/src/Volunteers.Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Volunteers.Domain.Volunteer.Models;
+
+namespace Volunteers.Infrastructure.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private const string VOLUNTEERS_CONNECTION_STRING = "VolunteersDbConnectionString";
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Volunteer> Volunteers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString(VOLUNTEERS_CONNECTION_STRING));
+            optionsBuilder.UseSnakeCaseNamingConvention();
+            optionsBuilder.UseLoggerFactory(CreateLoggerFactory());
+        }
+
+        private ILoggerFactory CreateLoggerFactory() =>
+             LoggerFactory.Create(builder => { builder.AddConsole(); });
+    }
+}
