1db8a00e950ba3cc4c558c7ae7a20f23b8fa00a3
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-13 17:27:53 +0300

    entity

---
+++ backend/src/Volunteers.Domain/Shared/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Volunteers.Domain.Shared;
+
+public abstract class Entity<TId> where TId : notnull 
+{
+    protected Entity(TId id)
+    {
+        Id = id;
+    }
+
+    public TId Id { get; private set; }
+}
