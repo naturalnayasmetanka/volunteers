a4239cc14a1747762ed93079fa7992cb16b83785
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-13 17:28:35 +0300

    update volunteer models

--- backend/src/Volunteers.Domain/Volunteer/Models/SocialNetwork.cs
+++ backend/src/Volunteers.Domain/Volunteer/Models/SocialNetwork.cs
@@ -1,3 +1,34 @@
-﻿namespace Volunteers.Domain.Volunteer.Models;
+﻿using CSharpFunctionalExtensions;
 
-public record SocialNetwork(string Title, string Link);
+namespace Volunteers.Domain.Volunteer.Models;
+
+public record SocialNetwork
+{
+    private SocialNetwork(
+        string title,
+        string link)
+    {
+        Title = title;
+        Link = link;
+    }
+
+    public string Title { get; }
+    public string Link { get; }
+
+    public static Result<SocialNetwork> Create(
+        string title,
+        string link)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            return Result.Failure<SocialNetwork>("Title can not be empty");
+
+        if (string.IsNullOrWhiteSpace(link))
+            return Result.Failure<SocialNetwork>("Link can not be empty");
+
+        var newSocial = new SocialNetwork(
+            title: title,
+            link: link);
+
+        return Result.Success(newSocial);
+    }
+}
