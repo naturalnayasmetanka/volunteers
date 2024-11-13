d47bd612385d0360450695cb5add4f6c090cf4e1
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-13 17:28:54 +0300

    update pet models

---
+++ backend/src/Volunteers.Domain/Pet/Models/PetDetails.cs
@@ -0,0 +1,22 @@
+﻿namespace Volunteers.Domain.Pet.Models
+{
+    public record PetDetailsLocation
+    {
+        public List<Location> Locations { get; private set; } = [];
+    }
+
+    public record PetDetailsRequisite
+    {
+        public List<PetRequisite> Requisites { get; private set; } = [];
+    }
+
+    public record PetDetailsPhoto
+    {
+        public List<PetPhoto> Photo { get; private set; } = [];
+    }
+
+    public record PetDetailsPhysicalParameters
+    {
+        public List<PhysicalParameters> PhysicalParameters { get; private set; } = [];
+    }
+}
