e9aba8f105f6a79a3cb8b1b1f46c13f2102b7468
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-13 17:28:09 +0300

    add temp configurations

---
+++ backend/src/Volunteers.Infrastructure/Configurations/PetConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Volunteers.Domain.Pet.Models;
+
+namespace Volunteers.Infrastructure.Configurations
+{
+    public class PetConfiguration : IEntityTypeConfiguration<Pet>
+    {
+        public void Configure(EntityTypeBuilder<Pet> builder)
+        {
+            builder.ToTable("pets");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .HasConversion(
+                    id => id.Value,
+                    value => PetId.Create(value)
+                );
+
+            builder.Property(x => x.Nickname)
+                .IsRequired(true)
+                .HasMaxLength(20);
+
+            builder.Property(x => x.CommonDescription)
+                .IsRequired(true)
+                .HasMaxLength(1000);
+
+            builder.Property(x => x.HelthDescription)
+                .IsRequired(true)
+                .HasMaxLength(1000);
+
+            builder.Property(x => x.PhoneNumber)
+                .IsRequired(true)
+                .HasMaxLength(15);
+
+            builder.Property(x => x.HelpStatus)
+                .IsRequired(true);
+
+            builder.Property(x => x.BirthDate)
+                .IsRequired(false);
+
+            builder.Property(x => x.CreationDate)
+                .IsRequired(true);
+        }
+    }
+}
---
+++ backend/src/Volunteers.Infrastructure/Configurations/VolunteerConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Volunteers.Domain.Volunteer.Models;
+
+namespace Volunteers.Infrastructure.Configurations
+{
+    public class VolunteerConfiguration : IEntityTypeConfiguration<Volunteer>
+    {
+        public void Configure(EntityTypeBuilder<Volunteer> builder)
+        {
+            builder.ToTable("volunteers");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .HasConversion(
+                    id => id.Value,
+                    value => VolunteerId.Create(value)
+                );
+
+            builder.Property(x => x.Name)
+                .IsRequired(true)
+                .HasMaxLength(50);
+
+            builder.Property(x => x.Email)
+                .IsRequired(true)
+                .HasMaxLength(50);
+
+            builder.Property(x => x.ExperienceInYears)
+                .IsRequired(true)
+                .HasMaxLength(4);
+
+            builder.Property(x => x.PhoneNumber)
+                .IsRequired(true)
+                .HasMaxLength(15);
+
+            builder.HasMany(x => x.Pets)
+                .WithOne()
+                .HasForeignKey("volunteer_id")
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
