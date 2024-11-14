3d479273317439143fb8620d3a0db5314793431d
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-14 12:45:44 +0300

    add breed models

---
+++ backend/src/Volunteers.Domain/Breed/Models/Breed.cs
@@ -0,0 +1,44 @@
+﻿using CSharpFunctionalExtensions;
+using CustomEntity = Volunteers.Domain.Shared;
+
+namespace Volunteers.Domain.Breed.Models;
+
+public class Breed : CustomEntity.Entity<BreedId>
+{
+    private Breed(BreedId id)
+        : base(id)
+    {
+    }
+
+    private Breed(
+        BreedId id,
+        string title,
+        string description
+        ) : base(id)
+    {
+        Title = title;
+        Description = description;
+    }
+
+    public string Title { get; private set; } = default!;
+    public string Description { get; private set; } = default!;
+
+    public static Result<Breed> Create(
+        BreedId id,
+        string title,
+        string description)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            return Result.Failure<Breed>("Title can not be empty");
+
+        if (string.IsNullOrWhiteSpace(description))
+            return Result.Failure<Breed>("Description can not be empty");
+
+        var newBreed = new Breed(
+            id,
+            title,
+            description);
+
+        return Result.Success(newBreed);
+    }
+}
