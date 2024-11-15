fa5c6045c428ef9bc5ff6ac115b247b54685a43b
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-15 10:16:24 +0300

    del setter VolunteerId

--- backend/src/Volunteers.Domain/Volunteer/Models/VolunteerId.cs
+++ backend/src/Volunteers.Domain/Volunteer/Models/VolunteerId.cs
@@ -7,7 +7,7 @@ public record VolunteerId
         Value = value;
     }
 
-    public Guid Value { get; set; }
+    public Guid Value { get; }
 
     public static VolunteerId NewVolunteerId() => new VolunteerId(Guid.NewGuid());
     public static VolunteerId EmptyVolunteerId() => new VolunteerId(Guid.Empty);
