8965d9ea14afab6340c243771c6a91a2796b4867
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-16 15:03:04 +0300

    add base controller

---
+++ backend/src/Volunteers.API/Controllers/VolunteerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volunteers.Application.Volunteer.CreateVolunteer;
+
+namespace Volunteers.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VolunteerController : ControllerBase
+{
+    [HttpPost]
+    public IEnumerable<string> Create(
+        [FromServices] CreateVolunteerHandler handler,
+        [FromBody] CreateVolunteerRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        return new string[] { "value1", "value2" };
+    }
+}
