e1d06b71225bba2c1d97b2bce04cb063c142eb0a
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-16 15:02:56 +0300

    add handlers

---
+++ backend/src/Volunteers.Application/Volunteer/CreateVolunteer/CreateVolunteerHandler.cs
@@ -0,0 +1,20 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace Volunteers.Application.Volunteer.CreateVolunteer;
+
+public class CreateVolunteerHandler
+{
+    private readonly IVolunteerRepository _repository;
+
+    public CreateVolunteerHandler(IVolunteerRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Result<Guid>> Handle(
+        CreateVolunteerRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        return Guid.Empty;
+    }
+}
---
+++ backend/src/Volunteers.Application/Volunteer/CreateVolunteer/CreateVolunteerRequest.cs
@@ -0,0 +1,5 @@
+﻿namespace Volunteers.Application.Volunteer.CreateVolunteer;
+
+public record CreateVolunteerRequest
+{
+}
