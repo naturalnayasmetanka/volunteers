6baa59b8f0c0e2ed20ddce2b2a3a4e5d5dd9c163
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-18 17:08:16 +0300

    update rep

--- backend/src/Volunteers.Infrastructure/Repositories/VolunteerRepository.cs
+++ backend/src/Volunteers.Infrastructure/Repositories/VolunteerRepository.cs
@@ -1,7 +1,24 @@
 ﻿using Volunteers.Application.Volunteer;
+using Volunteers.Domain.Volunteer.Models;
+using Volunteers.Infrastructure.Contexts;
 
 namespace Volunteers.Infrastructure.Repositories;
 
-public class VolunteerRepository: IVolunteerRepository
+public class VolunteerRepository : IVolunteerRepository
 {
+    private readonly ApplicationDbContext _context;
+
+    public VolunteerRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Volunteer> CreateAsync(Volunteer newVolunteer, CancellationToken cancellationToken = default)
+    {
+        await _context.Volunteers.AddAsync(newVolunteer, cancellationToken);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return newVolunteer;
+    }
 }
