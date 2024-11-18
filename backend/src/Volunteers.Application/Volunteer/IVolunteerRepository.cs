9ee1462dceda569c1be087f56050b85d1c1a534b
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-18 17:07:25 +0300

    update reps

--- backend/src/Volunteers.Application/Volunteer/IVolunteerRepository.cs
+++ backend/src/Volunteers.Application/Volunteer/IVolunteerRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace Volunteers.Application.Volunteer;
+﻿using VolunteerModel = Volunteers.Domain.Volunteer.Models.Volunteer;
+
+namespace Volunteers.Application.Volunteer;
 
 public interface IVolunteerRepository
 {
+    public Task<VolunteerModel> CreateAsync(VolunteerModel newVolunteer, CancellationToken cancellationToken = default);
 }
-
