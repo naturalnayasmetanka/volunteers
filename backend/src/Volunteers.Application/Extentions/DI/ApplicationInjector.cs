854ef3de9f8cc8f84aeaf89414ac3f4d34f320ba
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-11-19 17:03:14 +0300

    add validator

--- backend/src/Volunteers.Application/Extentions/DI/ApplicationInjector.cs
+++ backend/src/Volunteers.Application/Extentions/DI/ApplicationInjector.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using Volunteers.Application.Volunteer.CreateVolunteer;
 
 namespace Volunteers.Application.Extentions.DI;
@@ -8,6 +9,7 @@ public static class ApplicationInjector
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<CreateVolunteerHandler>();
+        services.AddValidatorsFromAssembly(typeof(ApplicationInjector).Assembly);
 
         return services;
     }
